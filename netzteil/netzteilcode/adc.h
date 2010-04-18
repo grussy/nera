b70b49f050cd738153395f1c7540d7605e4c215f
to.grussenmeyer <to.grussenmeyer@3b5921a4-2644-11df-93db-1366af54f32e>
2010-04-18 14:42:24 +0000

    t

--- netzteil/netzteilcode/adc.h
+++ netzteil/netzteilcode/adc.h
@@ -1,10 +1,15 @@
-#define ADCMAXINC 963
-#define ADCMAXVOL 1700
+#define ADCMAXINC 1023
+#define ADCMAXVOL 3300
+#define DACMAXINC 1004
+#define ADCMAXCUR 200
+#define ADCMAXCURINC	1000
 void setdac(int value);
 void init_ADC(void);
-int getadc(void);
-int getadc_ready(void);
-void setadc_ready(int ready);
+int getadc_u(void);
+int getadc_i(void);
 long getadc_Volts(void);
+long getadc_Current(void);
 long getVoltage(void);
 void setVoltage(long voltage100);
+long getCurrent(void);
+void setCurrent(long current100);
--- netzteil/netzteilcode/main.c
+++ netzteil/netzteilcode/main.c
@@ -11,11 +11,15 @@
 #include "adc.h"
 #include <util/delay.h>
 
+#define VOLTAGE_MAX 3300
+#define CURRENT_MAX 200
+
 
 int main(void)
 {
 	// Initialize Uart ..........................................
 	char buffer [20];
+	char buffer2 [20];
 	USART_Init();
 	//...........................................................
 
@@ -30,9 +34,10 @@ int main(void)
 	//...........................................................
 
 	// Initialize Drehgeber .....................................
-	int32_t drehgeber = 0;
-	DDRC &= ~((1<<DDC7) | (1<<DDC6)); // Set PORTC Pins 6 and 7 as input
-	PORTC |= (1<<PC7) | (1<<PC6); // Internal pullups
+	int32_t soll_voltage = 0;
+	int32_t soll_current = 0;
+	DDRC &= ~((1<<DDC7) | (1<<DDC6) | (1<<DDC0) | (1<<DDC1)); // Set PORTC Pins 6 and 7 as input
+	PORTC |= (1<<PC7) | (1<<PC6) | (1<<PC0) | (1<<PC1); // Internal pullups
 	encode_init();
 	//...........................................................
 
@@ -56,27 +61,50 @@ int main(void)
 			//uart_puts("zeichen vorhanden\n");
 			identify_command();
 		}
+		int test = encode_read4();
+		int test2 = encode_read2();
 
-		if(1)
+		if(test)
 		{
-			drehgeber += encode_read2();
-			setVoltage((long)drehgeber);
+			//int test = encode_read4();
+			sprintf(buffer, "test %i \n", test);
+			uart_puts(buffer);
+			if (test > 1) test = 100;
+			if (test < -1) test = -100;
+
+			soll_voltage += test; //lese die Voltage Drehgeber Veränderung
+
+			if (soll_voltage < 0) soll_voltage = 0; // keine negativen Werte zulassen
+			if (soll_voltage > VOLTAGE_MAX) soll_voltage = VOLTAGE_MAX; // nicht größer als das Maximum
+			setVoltage((long)soll_voltage);
 			//setdac(drehgeber);
-			sprintf(buffer, "Soll: %li,%li V          ", getVoltage() / 100, getVoltage() % 100);
+			long voltagesoll = getVoltage();
+			long voltageist = getadc_Volts();
+			sprintf(buffer, "U: %li,%li[%li,%li] V       ", voltagesoll / 100, voltagesoll % 100, voltageist / 100, voltageist % 100);
 			set_cursor(0,1);
 			lcd_string(buffer);
 		}
 
 
 
-		if (1)
+		if(test2)
 		{
+			//int test2 = encode_read2();
+			if (test2 > 1) test2 = 100;
+			if (test2 < -1) test2 = -100;
+
+			soll_current += test2; //lese die Voltage Drehgeber Veränderung
+
+			if (soll_current < 0) soll_current = 0; // keine negativen Werte zulassen
+			if (soll_current > CURRENT_MAX) soll_current = CURRENT_MAX; // nicht größer als das Maximum
+			setCurrent((long)soll_current);
+			//setdac(drehgeber);*/
+			long currentsoll = getCurrent();
+			long currentist = getadc_Current();
+
+			sprintf(buffer2, "I: %li,%li[%li,%li] A       ", currentsoll / 100, currentsoll % 100, currentist / 100, currentist % 100);
 			set_cursor(0,2);
-			sprintf(buffer, "Actual: %li,%li V          ", getadc_Volts() / 100, getadc_Volts() % 100);
-			lcd_string(buffer);
-			//uart_puts(buffer);
-			ADCSRA |= (1<<ADSC); // Start ADConversion
-			setadc_ready(0);
+			lcd_string(buffer2);
 		}
 	}
 }
